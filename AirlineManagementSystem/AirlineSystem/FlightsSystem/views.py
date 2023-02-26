dc05a388fab868cbeb583a8a24468927372c557f
Abdullah-max-eng <abdullahfazli8.af@gmail.com>
2023-02-26 23:03:19 +0300

    I added comment

--- AirlineManagementSystem/AirlineSystem/FlightsSystem/views.py
+++ AirlineManagementSystem/AirlineSystem/FlightsSystem/views.py
@@ -10,6 +10,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+# These are new changes
 
 
 def LoginPage(request):
